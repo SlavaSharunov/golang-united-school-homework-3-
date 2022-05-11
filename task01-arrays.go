ae85a03b282395bbd2017dc8c186184f5bfe5db9
Viacheslav Sharunov <sharunov@mst.ch>
2022-05-11 15:39:50 +0200

    Code refactored for home assignment 3

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,6 @@
 package homework
 
-func average(input [5]float32) (result float32) {
+func average(input [15]float32) (result float32) {
 	var sum float32
 	length := len(input)
 	for i := 0; i < length; i++ {
